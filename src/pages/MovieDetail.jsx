b312986cf5f736c36cbacfe70833e61aada1fd87
Sweatha <sweatha.2253054@srec.ac.in>
2025-08-19 11:29:37 +0530

    Add dynamic movie detail page using useParams

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+
 export default function MovieDetail(){
+
+    const params = useParams();
+    const [movie, setMovie] = useState(null);
+    useEffect(() => {
+        fetch(`/api/movies/${params.id}`)
+        .then(res => res.json())
+        .then(data => setMovie(data.movies))
+    },[params.id])
+
     return(
-        <h1>MovieDetail page</h1>
+        <div>
+            {
+                movie ? (
+                    <div>
+                    <img 
+                    className="w-[350px] h-[450px] object-cover rounded mb-2"
+                    src={movie.imageUrl} />
+                    <i className={`
+                        inline-block mt-1 px-4 py-1 rounded text-white
+                        ${movie.type === "comedy"  && "bg-orange-400"}
+                        ${movie.type === "thriller" && "bg-green-700"}
+                        ${movie.type === "horror"  && "bg-red-700"}
+                    `}>{movie.type}</i>
+                    <h2>{movie.name}</h2>
+                    <p><span>${movie.price}</span>/movie</p>
+                    <p>{movie.description}</p>
+                    <button 
+                    className="bg-blue-400 hover:bg-blue-500 shadow rounded px-2 py-2 text-white"
+                    >Buy this movie</button>
+                </div>
+                ) : <h1>Loading...</h1>
+            }
+        </div>
     )
 }
